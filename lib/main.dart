6477622313be15f8ed466a71e40d898b635eb43f
Zain Mustafa <zainmustafa@Zains-MacBook-Pro.local>
2019-05-18 23:36:33 -0400

    Changes to app routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import './pages/auth.dart';
+import 'pages/products_admin.dart';
+import 'pages/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +12,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            brightness: Brightness.light,
-            primarySwatch: Colors.deepOrange,
-            accentColor: Colors.deepPurple),
-        home: AuthPage());
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ProductsAdminPage()
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if (pathElements[1] == 'products') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                ProductPage(products[index]['title'], products[index]['image']),
+          );
+        }
+        return null;
+      },
+    );
   }
 }
